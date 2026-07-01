9a22b993b352706438f14523b67948e231ed48af
sadikh78 <sadikhshaik7866@gmail.com>
2026-07-01 18:09:55 +0530

    For,while,Dowhile loops

---
+++ Loops.java
@@ -0,0 +1,54 @@
+// looping statements //
+//For loop //
+import java.util.*;
+ class conditional{
+    public static void main ( String[] args){
+        for(int i=0;i<=20;i++){
+            System.out.println(i);
+        }
+    }
+ }
+         
+
+// looping statements //
+//Reverse for loop //
+import java.util.*;
+ class conditional{
+    public static void main ( String[] args){
+        for(int i=20;i>=0;i--){
+            System.out.println(i);
+        }
+    }
+ }
+
+
+// looping statements //
+// TABLE PRINTING //
+import java.util.*;
+ class conditional{
+    public static void main ( String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("enter the num:");
+        for(int i=0;i<=10;i++){
+            System.out.println(num  + " X " + i + " = " + (num*i));
+        }
+    }
+ }
+
+// looping statements //
+// REVERSE TABLE PRINTING //
+import java.util.*;
+ class conditional{
+    public static void main ( String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("enter the num:");
+        for(int i=10;i>=0;i--){
+            System.out.println(num  + " X " + i + " = " + (num*i));
+        }
+    }
+ }
+         
+         
+         
